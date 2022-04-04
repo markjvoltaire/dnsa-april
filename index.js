81220ab3794c98817694497134d215fdc302de32
Mark Voltaire <markjvoltaire@gmail.com>
2022-04-04 15:48:55 -0400

    fizzbuzz

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ function reverserWord(sentence) {
 const reverse = reverserWord('alchemy rocks gold');
 
 console.log('reverse', reverse);
+
+
+
+
+
+
+for (let i = 1; i < 16; i++) {
+  if(i % 15 === 0) {
+      console.log('fizzbuzz')
+  } 
+  else if (i % 3 === 0) {
+    console.log('Fizz')
+  } 
+  else if (i % 5 === 0) {
+    console.log('buzz')
+  } 
+  else {
+    console.log(i)
+  }
+}
